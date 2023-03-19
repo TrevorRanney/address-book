f24dd474e4d10c5419e8ffd6cca45b97b36aceee
Trevor Ranney <TrevorRanney@gmail.com>
2023-03-18 23:44:53 -0600

    Added search/filter to react address book app

--- react-address-book/src/App.js
+++ react-address-book/src/App.js
@@ -10,7 +10,8 @@ const App = () => {
 	const showCardView = 'Show Card View'
 	const showTableView = 'Show Table View'
 	const [nextView, changeView] = useState(showCardView)
-	const [data] = useState(addressBook.AddressBook.Contact)
+	const [data, setData] = useState(addressBook.AddressBook.Contact);
+	const [inputValue, setInputValue] = useState('');
 
 	const handleClick = () => {
 		switch(nextView){
@@ -22,8 +23,22 @@ const App = () => {
 		}
 	};
 
+	const filterContacts = (event) => {
+		setInputValue(event.target.value);
+		const data = addressBook.AddressBook.Contact.filter(contact => {
+			for(let prop in contact){
+				if(contact[prop].toLowerCase().includes(event.target.value.toLowerCase())){
+					return true;
+				}
+			}
+			return false;
+		})
+		setData(data);
+	};
+
 	return (
 		<div className='App'>
+			<label>Filter</label><input type="text" value={inputValue} onChange={filterContacts} />
 			<button onClick={handleClick}>{nextView}</button>
 			{nextView === showCardView ? <TableView addressBook={data} /> : <CardView addressBook={data} />}
 		</div>
